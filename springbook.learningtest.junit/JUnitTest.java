73a1063c5692b06bb7db797986dff4f2db2368ec
AM05307 <33053586+AM05307@users.noreply.github.com>
2022-02-20 22:31:55 +0900

    리스트 2-25 개선한 JUnit 테스트 오브젝트 생성에 대한 학습 테스트
    p. 202
    스태틱 변수로 테스트 오브젝트를 저장할 수 있는 컬렉션을 만들어둔다. 
    테스트 마다 현재 테스트 오브젝트가 컬렉션에 이미 등록되어 있는지 확인하고, 없으면 자기 자신을 추가. 
    이 과정을 반복. 
    이 방법을 이용하면 테스트가 어떤 순서로 실행되는 지에 상관없이 오브젝트 중복 여부를 확인할 수 있다. 
    이번에는 컬렉션의 원소인지를 검 사하는 hasItem() 매처를 사용.
    이렇게 만들어진 학습 테스트를 이용하면 JUnit이 매번 새로운 테스트 오브젝트를 만 든다는 사실을 분명히 확인할 수 있다.

--- springbook.learningtest.junit/JUnitTest.java
+++ springbook.learningtest.junit/JUnitTest.java
@@ -4,25 +4,27 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.junit.Assert.assertThat;
+import static org.junit.matchers.JUnitMatchers.hasItem;
+
+import java.util.Set;
 
 import org.junit.Test;
 
 public class JUnitTest {
-	static JUnitTest testObject;
-	
-	@Test public void test1() {
-		assertThat(this, is(not(sameInstance(testObject))));
+	static Set<JUnitTest> testObjects = new HashSet<JUnitTest>();
 		
-		testObject = this;
+	@Test public void test1() {
+		assertThat(testObejects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test public void test2() {
-		assertThat(this, is(not(sameTnstance(testObject))));
-		testObject = this;
+		assertThat(testObejects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 	
 	@Test public void test3() {
-		assertThat(this, is(not(sameInstance(testObject))));
-		testObject = this;
+		assertThat(testObejects, not(hasItem(this)));
+		testObjects.add(this);
 	}
 }
