36902dc3adefa504e115add470d3a73e9410381b
AM05307 <33053586+AM05307@users.noreply.github.com>
2022-02-20 21:52:13 +0900

    리스트 2-19 UserDao를 직접 DI 받도록 만든 테스트

--- UserDaoTest.java
+++ UserDaoTest.java
@@ -4,7 +4,7 @@ package a01;
 @ContextConfiguration(locations="/applicataion.xml") // 테스트 컨텍스트가 자동으로 만들어줄 애플리케이션 컨텍스트의 위치 지정
 public class UserDaoTest {
 	@Autowired
-	private ApplicationContext context;//테스트 오브젝트가 만들어지고 나면 스프링 테스트 컨텍스트에 의해 자동으로 값이 주입된다. 
+	UserDao dao; // UserDao 타입 빈을 직접 DI(의존성 주입) 받는다 
 	
 	@Before
 	public void setUp() { // @Before가 붙어있어서 매 테스트 메소드가 실행되기 전에 한번씩 실행된다. 
