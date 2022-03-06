e3057f7db8d9dc563b7cf57446f653dc96d521b2
AM05307 <33053586+AM05307@users.noreply.github.com>
2022-03-06 23:13:44 +0900

    리스트 5-7 새로운 필드를 포함하는 User 필드 값 검증 메소드
    두 개의 User 오브젝트 필드 값이 모두 같은지 비교하는 checkSameUser()메소드 수정

--- UserDaoTest.java
+++ UserDaoTest.java
@@ -1,5 +1,7 @@
 package a01;
 
+import springbook.user.domain.User;
+
 @RunWith(SpringJUnit4ClassRunner.class) // 스프링의 테스트 컨텍스트 프레임워크의 JUnit확장기능 지정 
 public class UserDaoTest {
 	@Autowired UserDao dao;
@@ -15,7 +17,7 @@ public class UserDaoTest {
 		dao.setDataSource(dataSource);
 		// 오브젝트 생성, 관계설정 등을 모두 직접 해준다.
 		
-	    this.user1 = new User("gyumee", "박성철", "springno1", Lvl.BASIC, 1, 0);
+		this.user1 = new User("gyumee", "박성철", "springno1", Lvl.BASIC, 1, 0);
 	    this.user2 = new User("leegw700", "이길원", "springno2", Lvl.SILVER, 55, 10);
 	    this.user3 = new User("bumjin", "박범진", "springno3", Lvl.GOLD, 100, 40);
 	}
@@ -25,6 +27,15 @@ public class UserDaoTest {
 		dao.deleteAll();
 		dao.add(user1);
 		dao.add(user1); //강제로 사용자를 두번 등록한다. 여기서 예외 발생 
+	}
+	
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
+		assertThat(user1.getLevel(), is(user2.getLevel()));
+		assertThat(user1.getLogin(), is(user2.getLogin()));
+		assertThat(user1.getRecommend(), is(user2.getRecommend()));
 	}
 
 }
