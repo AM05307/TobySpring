6c41ce22277aef1319ef5e0206a178f9baa5d055
AM05307 <33053586+AM05307@users.noreply.github.com>
2022-03-06 22:51:57 +0900

    리스트 5-4 User에 추가된 필드
    접근자와 수정자 메소드 추가

--- springbook.user.domain/User.java
+++ springbook.user.domain/User.java
@@ -1,13 +1,75 @@
 package springbook.user.domain;
 
 public class User {
-	private static final int BASIC = 1;
-	private static final int SILVER = 2;
-	private static final int GOLD = 3;
-	
-	int level;
-	
-	public void setLevel(int level) {
-		this.level =level;	
-	}
+    String id;
+    String name;
+    String password;
+    Level level;
+    int login;
+    int recommend;
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(String id, String name, String password, Level level, int login, int recommend) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+        this.level = level;
+        this.login = login;
+        this.recommend = recommend;
+    }
+
+    public User() { }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Level getLevel() {
+        return level;
+    }
+
+    public void setLevel(Level level) {
+        this.level = level;
+    }
+
+    public int getLogin() {
+        return login;
+    }
+
+    public void setLogin(int login) {
+        this.login = login;
+    }
+
+    public int getRecommend() {
+        return recommend;
+    }
+
+    public void setRecommend(int recommend) {
+        this.recommend = recommend;
+    }
 }
